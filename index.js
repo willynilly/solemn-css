d35f9702953b6155a4f8670ca37a8b93ba509ec7
Will Riley <will.riley@slalom.com>
2016-05-17 17:12:24 -0400

    removed main method

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ var _ = require('lodash');
 var profanity = require('profanity-util');
 var css = require('css');
 
-if (process.argv[2]) {
-    main();
-}
-
-function main() {
-    var filename = process.argv[2];
-    var codeTexts = parseCodeTexts(filename);
-    codeTexts = detectProfanity(codeTexts);
-    reportProfanityViolations(filename, codeTexts);
-}
-
 function addNewElementsToArray(oldArray, elements) {
     _.forEach(elements, function(e) {
         if (!_.find(oldArray, e)) {
